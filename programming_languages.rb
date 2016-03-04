48399859ff18bd40e19ad849ec28bc87e9d5613c
Cliff <cregisexcel@yahoo.com>
2016-03-04 14:45:53 +0000

    nitrous-backup at Fri Mar  4 14:45:53 UTC 2016

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,51 @@
+# # languages =
+# # {
+  # #   :oo =>
+  # # {
+  # #     :ruby => {
+  # #       :type => "interpreted"
+  # #     },
+  # #     :javascript => {
+  # #       :type => "interpreted"
+  # #     }
+  # #   },
+  # #   :functional =>
+  # #       {
+  # #
+  # #     :javascript => {
+  # #       :type => "interpreted"
+  # #     }
+  # #    }
+  # # },
+#ANSWER
+  # #     {
+  #   #       :ruby =>
+  #           {
+  #   #         :type => "interpreted",
+  #   #         :style => [:oo]
+  #   #       },
+  #   #       :javascript =>
+  #            {
+  #   #         :type => "interpreted",
+  #   #         :style => [:oo, :functional]
+  #   #       }
+  # #    }
+# # }
+
+#     Start by setting a variable, let's call it new_hash equal to an empty hash. Throughout your iteration over the languages hash, you can then add new key/value pairs to new_hash. At the end of your method, return new_hash.
+#     Remember that you can create key/value pairs by using the []= method on a hash. For example:
+
+#     hash = {}
+#     hash[:my_key] = "my value"
+
+#     puts hash
+#     #  => {my_key: "my value"}
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |key, val|
+      new_hash[key] = val
+  end
+      puts new_hash
 end
